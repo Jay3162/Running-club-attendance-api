f719dbebf4d34fb2bc90fc7735ad66593fa5b401
jamal <davisjamal647@gmail.com>
2026-03-29 17:04:18 +0100

    created delete endpoint, moved endpoints into new python file

--- api/app/run.py
+++ api/app/run.py
@@ -1,52 +1,27 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
-from datetime import date
+from fastapi import APIRouter
 from typing import List
-
+from api.app.run_repo import runCreate, runId
+from api.app import run_repo
 
 router = APIRouter(prefix="/runs", tags=["run"])
 
-#temporary state in memory
-runs_db: List["runCreate"] = []
-next_run_id = 1
+@router.put("/", response_model=runId)
+def update_run(run: runId):
+    return run
 
-class runCreate(BaseModel):
-    # id: int
-    user_id: int
-    date: date
-    distance: float
-
-# plug in runCreate model to avoid repeatition and valid users input without cleaning it
-class runId(runCreate):
-    id: int
+@router.delete("/", response_model=runId)
+def remove_run(run_id: int):
+    return run_repo.delete_run(run_id)
 
 @router.post("/", response_model=runId)
 def runner(run: runCreate):
-    # run id needs to be referenced as a global variable
-    global next_run_id
-    if len(runs_db) >= 0:
-        # use the new model and set the id, the dump the existing run as a dictionary inside
-        run = runId(id=next_run_id, **run.dict())
-        runs_db.append(run)
-        next_run_id += 1
-
-    else:
-        run = runId(id=next_run_id, **run.dict())
-        runs_db.append(run)
-        next_run_id += 1
-
-    return run
-
-@router.get("/", response_model=List[runId])
-def get_run():
-    return runs_db
+    return run_repo.create_run(run)
+    
 
 @router.get("/{run_id}", response_model=runId)
 def run(run_id: int):
-    for run in runs_db:
-        if run.id == run_id:
-            return run
-    raise HTTPException(
-        status_code=404,
-        detail="user not found"
-    )
+    return run_repo.get_run(run_id)
+
+@router.get("/", response_model=List[runId])
+def get_run():
+    return run_repo.simple_get_run(run_repo.runs_db)
---
+++ api/app/run_repo.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from datetime import date
+from typing import List
+
+router = APIRouter(prefix="/runs", tags=["run"])
+
+#temporary state in memory
+runs_db: List["runCreate"] = []
+next_run_id = 1
+
+class runCreate(BaseModel):
+    user_id: int
+    date: date
+    distance: float
+
+# plug in runCreate model to avoid repeatition and valid users input without cleaning it
+class runId(runCreate):
+    id: int
+
+#delete
+def delete_run(run_id: int):
+    for run in runs_db:
+        if run_id == run.id:
+            rm_val = runs_db.index(run)
+            runs_db.pop(rm_val)
+            return run
+    raise HTTPException(
+        status_code=404,
+        detail="user not found"
+    )
+#post
+def create_run(run: runCreate):
+    global next_run_id
+    # use the new model and set the id, the dump the existing run as a dictionary inside
+    run = runId(id=next_run_id, **run.dict())
+    runs_db.append(run)
+    next_run_id += 1
+
+    return run
+
+#get
+def get_run(run_id: int):
+    for run in runs_db:
+        if run.id == run_id:
+            return run
+    raise HTTPException(
+        status_code=404,
+        detail="user not found"
+    )
+#simple get
+def simple_get_run():
+    return runs_db
+    
